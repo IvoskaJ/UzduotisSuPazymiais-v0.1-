b207e2ffd5a2d19ab719b21f28fb8e86b43efdab
IvoskaJ <78845735+IvoskaJ@users.noreply.github.com>
2021-05-12 09:04:36 +0300

    Add files via upload
    Versija 1.0

---
+++ vector1.cpp
@@ -0,0 +1,26 @@
+#include "vector1.h"
+#include <chrono>
+#pragma GCC optimize("Ofast")
+using namespace std;
+
+int main() {
+    time_t t;
+    srand((unsigned)time(&t));
+    vector<Studentas>* studentas = new vector<Studentas>;
+    char sutikimas;
+    int studentuSkaicius = 0;
+
+    cout << "jei norite patys pasirinkti duomenis iveskite t, o jei norite kad duomenys butu automatiskai sugeneruojami ir isskirstomi pagal studentu gabumus, iveskite n." << endl;
+    sutikimas = inputChar();
+    if (sutikimas == 't' || sutikimas == 'T') {
+        rankinisGeneravimas(*studentas, studentuSkaicius, sutikimas);
+    }
+    else {
+        for (int i = 1000; i <= 10000000; i = i * 10) {
+            cout << "pradedamas darbas su " << i << " dydzio failu." << endl;
+            workWithGeneratedFile(i);
+            cout << "failas uzdaromas, tesiama su 10 kartu didesniu failu." << endl;
+        }
+    }
+    return 0;
+}
